ddc419dac8cdc4df75ca9e70555709d74b399ac3
nj8627 <nitin.jindal@zsassociates.com>
2016-06-21 19:39:03 +0530

    mp12

--- d3_account/urls.py
+++ d3_account/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from d3_account import views
 
 urlpatterns = patterns('',
-#		url(r'^new/$', views.index, name='home_2'),
+		url(r'^new/$', views.index, name='home_2'),
 		
 )
 
