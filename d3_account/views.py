53e4ac59bbe4f33928f7fe40d524006006f947af
nj8627 <nitin.jindal@zsassociates.com>
2016-06-21 19:29:44 +0530

    mp11

--- d3_account/views.py
+++ d3_account/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext, loader
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def index(request):
+    return render_to_response('index.html',{},context_instance = RequestContext(request))
+
